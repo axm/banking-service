9698cba0bacda57ba1ec5eb1cf1b767d7d71c5a1
Alex M <amocanu2000@gmail.com>
2017-06-20 15:00:03 +0100

    Added DayMonth struct. To be used (potentially) with DirectDebit

---
+++ Banking.Domain/DayMonth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Domain
+{
+    public struct DayMonth : IEquatable<DayMonth>, IComparable<DayMonth>
+    {
+        public int Day { get; private set; }
+        public Month Month { get; private set; }
+
+        public DayMonth(int day, Month month)
+        {
+            Day = day;
+            Month = month;
+        }
+
+        public int CompareTo(DayMonth other)
+        {
+            if(Month < other.Month || (Month == other.Month && Day < other.Day))
+            {
+                return -1;
+            }
+
+            if(Month.Equals(other))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+
+        public bool Equals(DayMonth other)
+        {
+            return Month == other.Month && Day == other.Day;
+        }
+    }
+}
