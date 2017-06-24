13b5d38ca2ed79a7d4284eb5725a908071130648
Alex M <amocanu2000@gmail.com>
2017-06-24 21:52:53 +0100

    Removed unused projects

--- Credits.Domain/TransactionGuid.cs
+++ Credits.Domain/TransactionGuid.cs
@@ -1,9 +1,5 @@
-﻿using Common;
+﻿using Base.Types;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Credits.Domain
 {
